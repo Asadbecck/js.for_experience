e0f390e789629d89197eeb7524fa752fe788914b
Asadbecck <umirzakovasad83@gmail.com>
2022-09-30 19:13:42 +0500

    asad

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.getElementById('check').onclick = function(){
+// let login = document.getElementById('login').value;
+// let password = document.getElementById('password').value;
+
+// if(login == "user" && password == "admin"){
+//     alert('Welcome')
+// }
+// else{
+//     alert('Oups...')
+// }
+
+// }
+
+// let text = prompt("Sportchi va orin tanlang")
+// alert('Sizni sportchingiz '+ text + ' chi orin qolga kiriti!')                                                   
+
+
+
+calcAge(6) 
